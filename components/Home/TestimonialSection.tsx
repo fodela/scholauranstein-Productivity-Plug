be04fbcc2fd7cea8fd15b1f7c4b272c101d6446e
fodela <particleme30@gmail.com>
2024-02-12 08:50:40 +0000

    feat: add testimonial section component

---
+++ components/Home/TestimonialSection.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const TestimonialSection = () => {
+  return (
+    <section className="py-16  ">
+      <div className="text-center text-primary max-w-xl  mx-auto">
+        <p className="text-xl mb-4">
+          {" "}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          quisquam voluptatem exercitationem cum mollitia ipsum delectus
+          similique atque ipsam labore autem maiores nulla, facilis maxime harum
+          aut pariatur ab laboriosam.
+        </p>
+        <p>
+          Dr. Delali Dogbevi,{" "}
+          <span className="text-grey_main">Eastern Regional Hospital</span>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default TestimonialSection;
