13d79564871a2efd699ea6a3b4be0711ead132b6
fodela <particleme30@gmail.com>
2024-02-12 08:53:39 +0000

    style: add theme update react-icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        primary:"#080808",
+        secondary:"#e3cbcb",
+        grey_accent:"#9999",
+        grey_main:"#929292",
+      }
     },
   },
   plugins: [],
