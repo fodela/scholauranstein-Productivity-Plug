3e18395d6b76d76c83b6da4d8e82a2849f027101
fodela <particleme30@gmail.com>
2024-02-12 08:51:13 +0000

    refactor: accordion

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,50 +6,55 @@ import { IoIosMenu, IoMdClose } from "react-icons/io";
 const Header = () => {
   const [open, setOpen] = useState(false);
   return (
-    <header className="container flex justify-between items-center my-10 px-10 relative">
-      <Image
-        src="images/ComforTablelogo.svg"
-        alt="comforTable logo"
-        width={204.67}
-        height={44.46}
-        className="dark:invert"
-      />
-      <div className="md:flex gap-6 items-center hidden ">
-        <ul className="flex gap-6">
-          <li>Home</li>
-          <li>Product</li>
-          <li>Testimonials</li>
-          <li>Contact</li>
-        </ul>
+    <div className="fixed top-0 right-0 left-0 ">
+      <header className=" container px-4 flex justify-between items-center py-8 relative bg-white">
+        <Image
+          src="images/ComforTablelogo.svg"
+          alt="comforTable logo"
+          width={204.67}
+          height={44.46}
+          className="dark:invert"
+        />
+        <div className="md:flex gap-6 items-center hidden ">
+          <ul className="flex gap-6">
+            <li>Home</li>
+            <li>Product</li>
+            <li>Testimonials</li>
+            <li>Contact</li>
+          </ul>
 
-        <button className="px-4 py-2 bg-[#21211D] text-white dark:bg-white dark:text-[#21211D]  rounded-lg">
-          Order Now
-        </button>
-      </div>
-      {open ? (
-        <IoMdClose
-          className="md:hidden rotate90"
-          size={30}
-          onClick={() => setOpen(false)}
-        />
-      ) : (
-        <IoIosMenu
-          className="md:hidden anti-rotate90"
-          size={30}
-          onClick={() => setOpen(true)}
-        />
-      )}
-      {open && (
-        <ul className=" w-2/3 absolute flex flex-col top-10 -left-5 backdrop-blur-sm bg-[#21211dbf] text-white text-3xl gap-6 p-10 slideRight transition-transform text-center">
-          <li className="hover:text-black hover:bg-white transition-all  ">
-            Home
-          </li>
-          <li>Product</li>
-          <li>Testimonials</li>
-          <li>Contact</li>
-        </ul>
-      )}
-    </header>
+          <button className="px-4 py-2 bg-[#21211D] text-white dark:bg-white dark:text-[#21211D]  rounded-lg">
+            Order Now
+          </button>
+        </div>
+        {open ? (
+          <IoMdClose
+            className="md:hidden rotate90"
+            size={30}
+            onClick={() => setOpen(false)}
+          />
+        ) : (
+          <div className="flex uppercase item-center md:hidden text-primary text-sm font-medium gap-1">
+            menu{" "}
+            <IoIosMenu
+              className="anti-rotate90"
+              onClick={() => setOpen(true)}
+              size={20}
+            />
+          </div>
+        )}
+        {open && (
+          <ul className=" w-2/3 absolute flex flex-col top-10 -left-5 backdrop-blur-sm bg-[#21211dbf] text-white text-3xl gap-6 p-10 menuSlideRight transition-transform text-center">
+            <li className="hover:text-black hover:bg-white transition-all  ">
+              Home
+            </li>
+            <li>Product</li>
+            <li>Testimonials</li>
+            <li>Contact</li>
+          </ul>
+        )}
+      </header>
+    </div>
   );
 };
 
