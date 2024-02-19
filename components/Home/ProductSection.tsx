3eac1b77138d124f3d894dfcc3ae3fde1a31eb31
fodela <particleme30@gmail.com>
2024-02-19 06:18:03 +0000

    style: animate product hover

--- components/Home/ProductSection.tsx
+++ components/Home/ProductSection.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { Montserrat } from "next/font/google";
+import { FaHeart } from "react-icons/fa";
+import { IoIosHeart } from "react-icons/io";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -38,23 +40,42 @@ const ProductSection = () => {
       id="products"
       className="px-8 max-w-screen-xl mx-auto my-16 scroll-mt-28"
     >
-      <h2 className="font-bold text-xl mb-8 text-primary">Our Products</h2>
+      <h2 className="font-bold text-xl mb-8 text-primary text-center">
+        Our Products
+      </h2>
       <div className="grid md:grid-cols-2 xl:grid-cols-4 gap-8 justify-items-center ">
         {products.map((product) => (
-          <div key={product.id}>
-            <div className="bg-[#e3cbcb]/30 w-[288px] h-[348px] flex items-center justify-center">
+          <div key={product.id} className="group">
+            <div className="bg-[#e3cbcb]/30 w-[288px] h-[348px] flex items-center justify-center relative overflow-hidden">
               <Image
                 src={`/images/product/${product.image}`}
                 width={250}
                 height={250}
                 alt={`image ${product.name}`}
               />
+              <div className="flex absolute group-hover:translate-y-0 translate-y-full bottom-0 transition-all duration-150 ease-linear">
+                <p className="uppercase px-4 py-1 text-xs bg-primary text-white tracking-wide">
+                  quick look
+                </p>{" "}
+                <div className="py-1 px-2 bg-[#555] text-white hover:text-red-400 transition-colors ">
+                  <IoIosHeart size={15} />
+                </div>
+              </div>
             </div>
             <div className="text-center mt-6 mb-2">
               <h4 className="text-primary font-medium h-12 overflow-hidden">
                 {product.name}
               </h4>
-              <p className={montserrat.className}>GHC {product.price}</p>
+              <p className="relative overflow-hidden">
+                <span className="absolute uppercase inline-block transition-all ease-in hover:text-primary text-sm -translate-x-28 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 duration-300 cursor-pointer">
+                  add to cart
+                </span>{" "}
+                <span
+                  className={`${montserrat.className} inline-block group-hover:translate-x-28 group-hover:opacity-0 transition-all ease-out duration-300  `}
+                >
+                  GHC {product.price}
+                </span>
+              </p>
             </div>
           </div>
         ))}
