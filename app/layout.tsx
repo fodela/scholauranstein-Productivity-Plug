a94dbcab55b2b10b2e91f6948b0b2fe46eecf74e
fodela <particleme30@gmail.com>
2024-03-01 10:47:58 +0000

    feat: shadcn-ui init

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import Header from "@/components/Header";
 import "./globals.css";
 import { Catamaran } from "next/font/google";
 import Image from "next/image";
-import { FaInstagram } from "react-icons/fa";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
+import Link from "next/link";
 
 const catamaran = Catamaran({
   subsets: ["latin"],
@@ -38,7 +39,12 @@ export default function RootLayout({
               <p className="text-sm my-2">Where productivity meets comfort</p>
             </div>
             <div className="flex gap-2 self-center justify-center">
-              <FaInstagram size={25} />
+              <Link href="https://instagram.com/@scholauranstein">
+                <FaInstagram size={25} />
+              </Link>
+              <Link href="https://instagram.com/@scholauranstein">
+                <FaWhatsapp size={25} />
+              </Link>
             </div>
             <button className=" px-6 py-2 bg-white text-black   rounded-lg ">
               Order now
