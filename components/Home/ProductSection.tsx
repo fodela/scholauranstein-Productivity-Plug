49ac0f46f729cac4e98a7fca12d8058a03fd7f0c
fodela <particleme30@gmail.com>
2024-03-12 14:27:43 +0000

    refactor: product for mobile

--- components/Home/ProductSection.tsx
+++ components/Home/ProductSection.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import { Montserrat } from "next/font/google";
-import { FaHeart } from "react-icons/fa";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { IoIosHeart } from "react-icons/io";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -14,24 +16,28 @@ const products = [
     name: "Foldable bed table with USB, fan and light",
     price: "160",
     image: "bgtable.png",
+    isFavorite: false,
   },
   {
     id: 2,
     name: "Plain Foldable table",
     price: "120",
     image: "plainbedtable.png",
+    isFavorite: false,
   },
   {
     id: 3,
     name: "Laptop stand",
     price: "150",
     image: "laptopstanddetail.png",
+    isFavorite: false,
   },
   {
     id: 4,
     name: "Multipurpose wall mount accessory holder",
     price: "25",
     image: "bgwallmount.png",
+    isFavorite: false,
   },
 ];
 const ProductSection = () => {
@@ -46,36 +52,44 @@ const ProductSection = () => {
       <div className="grid md:grid-cols-2 xl:grid-cols-4 gap-8 justify-items-center ">
         {products.map((product) => (
           <div key={product.id} className="group">
-            <div className="bg-[#e3cbcb]/30 w-[288px] h-[348px] flex items-center justify-center relative overflow-hidden">
-              <Image
-                src={`/images/product/${product.image}`}
-                width={250}
-                height={250}
-                alt={`image ${product.name}`}
-              />
-              <div className="flex absolute group-hover:translate-y-0 translate-y-full bottom-0 transition-all duration-150 ease-linear">
-                <p className="uppercase px-4 py-1 text-xs bg-primary text-white tracking-wide">
-                  quick look
-                </p>{" "}
-                <div className="py-1 px-2 bg-[#555] text-white hover:text-red-400 transition-colors ">
-                  <IoIosHeart size={15} />
+            <Link href={`/products/${product.name}`}>
+              <div className="bg-secondary/50 w-[288px] h-[348px] flex items-center justify-center relative overflow-hidden">
+                <Image
+                  src={`/images/product/${product.image}`}
+                  width={250}
+                  height={250}
+                  alt={`image ${product.name}`}
+                />
+                <div className="flex absolute sm:group-hover:translate-y-0 translate-y-full bottom-0 transition-all duration-150 ease-linear">
+                  <p className="uppercase px-4 py-1 text-xs bg-primary text-white tracking-wide">
+                    quick look
+                  </p>{" "}
+                  <div className="py-1 px-2 bg-[#555] text-white hover:text-red-400 transition-colors ">
+                    <IoIosHeart size={15} />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="text-center mt-6 mb-2">
+            </Link>
+            <div className="flex flex-col items-center text-center mt-6 mb-2">
               <h4 className="text-primary font-medium h-12 overflow-hidden">
                 {product.name}
               </h4>
-              <p className="relative overflow-hidden">
-                <span className="absolute uppercase inline-block transition-all ease-in hover:text-primary text-sm -translate-x-28 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 duration-300 cursor-pointer">
+              <p className="relative overflow-hidden w-full">
+                <span className="absolute uppercase inline-block transition-all ease-in hover:text-primary text-sm -translate-x-28 opacity-0 sm:group-hover:translate-x-0 sm:group-hover:opacity-100 duration-300 cursor-pointer">
                   add to cart
                 </span>{" "}
                 <span
-                  className={`${montserrat.className} inline-block group-hover:translate-x-28 group-hover:opacity-0 transition-all ease-out duration-300  `}
+                  className={`${montserrat.className} inline-block sm:group-hover:translate-x-28 sm:group-hover:opacity-0 transition-all ease-out duration-300  `}
                 >
                   GHC {product.price}
                 </span>
               </p>
+              <div className="sm:hidden flex items-center gap-8 my-4 ">
+                <Button variant="primary">Add to cart</Button>
+                <div className="p-2 bg-secondary/30 rounded text-primary">
+                  <FaRegHeart size={20} />
+                </div>
+              </div>
             </div>
           </div>
         ))}
