4b1c546508c9f9a6984bed404b438d0d8fb04db0
fodela <particleme30@gmail.com>
2024-04-02 17:30:13 +0000

    style: footer container

--- app/(web)/layout.tsx
+++ app/(web)/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <Header />
       {children}
       <footer className="flex flex-col gap-4 divide-y  bg-primary p-10 ">
-        <div className="flex flex-wrap justify-between items-center gap-4 ">
+        <div className="container flex flex-wrap justify-between items-center gap-4 ">
           <div className="">
             <Image
               src="/images/logo.svg"
@@ -41,7 +41,7 @@ export default function RootLayout({
             Order now
           </button>
         </div>
-        <div className="sm:flex justify-between text-xs py-2">
+        <div className="container sm:flex justify-between text-xs py-2">
           <p>designed by KalaIT</p>
           <p>&copy; Copyright 2025 - Scholauranstein Productivity Plug</p>
         </div>
